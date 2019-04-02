24e2b728eb1e9b3f9fb90a163d606ae1e3593008
Ajordon11 <38749930+Ajordon11@users.noreply.github.com>
2019-04-02 15:49:35 +0200

    Final

--- main.m
+++ main.m
@@ -1,3 +1,4 @@
+
 clear
 load('dataonly.txt');
 %x = dataonly(:,3:8); 
@@ -7,7 +8,7 @@ y = transpose(dataonly(:,20));
 
 % vytvorenie štruktúry NS
 
-pocet_neuronov=10;
+pocet_neuronov=18;
 net=patternnet(pocet_neuronov,'trainlm');
  
 % % vyber rozdelenia
@@ -24,21 +25,47 @@ net.divideParam.valRatio=0;
 net.divideParam.testRatio=0.4;
 
 % Nastavenie parametrov trénovania
-net.trainParam.goal = 1e-10;     % Ukoncovacia podmienka na chybu
+net.trainParam.goal = 1e-7;     % Ukoncovacia podmienka na chybu
 net.trainParam.show = 5;        % Frekvencia zobrazovania priebehu chyby trénovania net.trainParam.epochs = 100;  % Max. po?et trénovacích cyklov.
-net.trainParam.epochs = 1000;    % maximalny pocet trenovacich epoch.
- 
+net.trainParam.epochs = 1500;   % maximalny pocet trenovacich epoch.
+net.trainParam.min_grad = 1e-8;
+
+for i=1:10
+    net=patternnet(pocet_neuronov,'trainlm');
+    net.divideFcn='dividerand';
+    net.divideParam.trainRatio=0.6;
+    net.divideParam.valRatio=0;
+    net.divideParam.testRatio=0.4;
+    net.trainParam.goal = 1e-7;     % Ukoncovacia podmienka na chybu
+    net.trainParam.show = 5;        % Frekvencia zobrazovania priebehu chyby trénovania net.trainParam.epochs = 100;  % Max. po?et trénovacích cyklov.
+    net.trainParam.epochs = 1500;   % maximalny pocet trenovacich epoch.
+    net.trainParam.min_grad = 1e-8;
+
 % Trénovanie NS
-net=train(net,x,y);
+    net=train(net,x,y);
  
 % Simulácia výstupu NS
-outnetsim = sim(net,x);
+    outnetsim = sim(net,x);
  
 % vypocet chyby siete
-SSE=sum((y-outnetsim).^2);
-MSE=SSE/length(y);
+    SSE=sum((y-outnetsim).^2);
+    MSE=SSE/length(y);
  
+% [c,cm,ind,per] = confusion(y, outnetsim);
 % Vykreslenie priebehov 
 
-figure
-plotconfusion(y,outnetsim);
+    figure(i)
+    plotconfusion(y,outnetsim);
+    pause(0.5);
+end
+
+testVector = randi(1151,10,1);          % vyber 10 nahodnych cisel z rozsahu 1-1151
+testInput = transpose(dataonly(testVector,1:19));   %vyber vstupnych dat podla nahodnych indexov
+testOutput = transpose(dataonly(testVector,20));    %vyber vystupnych dat
+
+test = sim(net,testInput);                          % testovanie 10 nahodnych vzoriek
+%vec2 = vec2ind(sim,10);
+figure(11);
+for i=1:10
+    plotconfusion(testOutput,test); %confusion matica nahodnej vzorky
+end
